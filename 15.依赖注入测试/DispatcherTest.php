0c74652990cda5ff24ba21cad2fda4b27e887666
Lei Shi <sjtushi@gmail.com>
2018-07-04 13:29:48 +0800

    第四周的挑战答案

---
+++ 15.依赖注入测试/DispatcherTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace test;
+
+define('APP_PATH',dirname(__DIR__));
+
+use PHPUnit\Framework\TestCase;
+use dispatcher\Container;
+
+class DispatcherTest extends TestCase
+{
+    private static $obj;
+
+    public static function setUpBeforeClass()
+    {
+	self::$obj = new Say();
+    }
+
+    public function testSayHi()
+    {
+	$this->assertEquals('Hi', self::$obj->hi());
+    }
+ 
+    public function testSayHello()
+    {
+	$this->assertEquals('Hello', call::hello());
+    }
+    public static function setDownAfterClass()
+    {
+	self::$obj = null;
+    }
+
+}
+class Foo extends Container
+{
+    protected function sayHi(Say $say)
+    {
+        return $say->hi();
+    }
+    public function sayHello(Say $say)
+    {
+        return $say->hello();
+    }
+}
+class Say extends Container
+{
+    public function hi()
+    {
+        return 'Hi';
+    }
+    public function hello()
+    {
+        return 'Hello';
+    }
+}
